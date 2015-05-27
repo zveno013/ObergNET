dcc096e114035147a68457bb5a5c3e0d61c91618
zveno013 <zveno013@ya.ru>
2015-05-27 16:33:05 +0400

    Ввод вывод
    Демонстрация ввода вывода с классом сборщика мусора

--- obergNET/obergNET/obergNET.cpp
+++ obergNET/obergNET/obergNET.cpp
@@ -1,6 +1,46 @@
 #using <mscorlib.dll>
 
+using namespace System;  //пространство имен System
+
+//class сборщика мусора
+__gc class InputWrapper
+{
+public:
+	int getInt(String *pprompt)  //Строка
+	{
+		Console::Write(pprompt);
+		String *pbuf = Console::ReadLine();
+		return Convert::ToInt32(pbuf); 
+	}
+	double getDouble(String *pprompt)
+	{
+		Console::Write(pprompt);
+		String *pbuf = Console::ReadLine();
+		return Convert::ToDouble(pbuf);
+	}
+	Decimal getDecimal(String *pprompt)
+	{
+		Console::Write(pprompt);
+		String *pbuf = Console::ReadLine();
+		return Convert::ToDecimal(pbuf);
+	}
+	String *getString(String *pprompt)
+	{
+		Console::Write(pprompt);
+		String *pbuf = Console::ReadLine();
+		return pbuf;
+	}
+};
+
 void main(void)
 {
-	System::Console::WriteLine("Привет (:");
+	InputWrapper *piw = new InputWrapper;
+	int numTemp = piw->getInt("How many temp's? ");
+	for (int i=0; i<numTemp; i++)
+	{
+		int fahr = piw->getInt("Temp. (Fahrenheit): ");
+		int celsius = (fahr - 32) * 5 / 9;  //Цельсия
+		Console::WriteLine("Fahrenheit = {0}", fahr.ToString());
+		Console::WriteLine("Celsius = {0}", __box(celsius));
+	}
 }
